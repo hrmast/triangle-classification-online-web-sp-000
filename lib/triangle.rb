bafc0be9b20398754843195f2a7112fa1f624a97
Heath Mast <hrmast@gmail.com>
2020-09-01 00:38:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
+require "pry"
 class Triangle
-  # write code here
+  attr_accessor :x, :y, :z, :valid, :valid2
+
+
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
+  end
+
+  def kind
+    if x == y && y == z && z == x
+      :equilateral
+    elsif x != y && y != z && x != z
+      :scalene
+    else
+      :isosceles
+    end
+    #binding.pry
+  end
+
+  def self.valid(x, y, z)
+    (x + y) > z && (x + z) > y && (y + z) > x ? true : false
+  end
+
+  def self.valid2(x,y,z)
+    x > 0 && y > 0 && z > 0 ? true : false
+  end
+
 end
